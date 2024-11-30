268f6f3fd83dc39f019e5434b8108d03df18a7fa
Robert Szlufik <L00188394@atu.ie>
2024-11-30 16:32:31 +0000

    added class outside of suite

--- src/test/java/vehicles/CarTest.java
+++ src/test/java/vehicles/CarTest.java
@@ -1,37 +1,21 @@
 package vehicles;
 
-import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@Tag("vehicles")
-class CarTest {
+public class CarTest {
 
     private Car car;
 
-    @Test
-    void getNumberOfDoors() {
-        // given
-        int numberOfDoors = 5;
-        // when
-        this.car = new Car(numberOfDoors);
-        // then
-        assertEquals(numberOfDoors, this.car.getNumberOfDoors());
-        assertNotEquals(numberOfDoors - 1, this.car.getNumberOfDoors());
+    @BeforeEach
+    void setup() {
+        car = new Car();
     }
 
     @Test
-    void setNumberOfDoors() {
-        // given
-        int numberOfDoors = 5;
-        this.car = new Car();
-        // when
-        this.car.setNumberOfDoors(numberOfDoors);
-        // then
-        assertEquals(numberOfDoors, this.car.getNumberOfDoors());
-        assertNotEquals(numberOfDoors - 1, this.car.getNumberOfDoors());
+    void testSetNumberOfDoors() {
+        car.setNumberOfDoors(2);
     }
-
-
 }
+
+
